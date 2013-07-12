74cfc8c4c137c36da7bb45c60871cf97d6f3ee03
Remi Ogunsanya <afrogizmo@gmail.com>
2013-07-12 01:58:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World2!');
-    //response.send(text);
+    
     });
 
 var port = process.env.PORT || 5000;
