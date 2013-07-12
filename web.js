6227cbb27b8bc0d4b50860912793776a43ab5410
Remi Ogunsanya <afrogizmo@gmail.com>
2013-07-12 02:17:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
 
-var text = fs.readFileSync('index.html','utf8')
+var fs = require('fs');
+
+var text = fs.readFileSync("/index.html","utf8");
 
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-    
-    });
+  //response.send('Hello World2!');
+    response.send(text);
+   });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
